caa93f8db1005937ce0db67f7db2b15a5d148ff4
ykosbie <ykosbie@gmail.com>
2018-04-25 20:47:27 +0300

    Add existing EfLinqDemo proj

---
+++ EfLinqDemo/EfLinqDemo.CLI/Program.cs
@@ -0,0 +1,25 @@
+﻿using EfLinqDemo.DataAccess.DataSource;
+using System;
+using System.Linq;
+
+namespace EfLinqDemo.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NorthwindContext ctx = new NorthwindContext())
+            {
+                foreach (var product in ctx.Products.Where(p => p.UnitsInStock.HasValue && p.UnitsInStock.Value >= 5))
+                {
+                    Console.WriteLine($"Product Name: {product.ProductName}, In Stock: {product.UnitsInStock ?? 0}");
+                }
+
+                var cntProducts = ctx.Products.Count();
+                Console.WriteLine($"There {cntProducts} products in the Northwind DB.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EfLinqDemo/EfLinqDemo.DataAccess/DataSource/NorthwindContext.cs
@@ -0,0 +1,11 @@
+﻿using EfLinqDemo.DataAccess.Models;
+using System.Data.Entity;
+
+namespace EfLinqDemo.DataAccess.DataSource
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
