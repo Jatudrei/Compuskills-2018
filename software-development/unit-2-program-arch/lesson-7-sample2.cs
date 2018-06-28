96a04e4218ae1af551f055141d6670ef20a4457f
ykosbie <ykosbie@sorissoftware.com>
2018-06-28 21:07:20 +0300

    Add more examples to lesson 7 outline

---
+++ software-development/unit-2-program-arch/lesson-7-sample2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class G
+{
+	public static int OffsetSplit(Point pointX, Point pointY)
+    {
+        var a = Math.Pow(pointX.X + pointY.X, 2);
+        var b = Math.Pow(pointX.Y + pointY.Y, 2);
+
+        return Math.Sqrt(a + b);
+    }
+}
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
